f87e8ed7dcd5837399ba9f1701740a52f5fcae7d
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-17 19:20:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
 
 
 /*
 ***************************************************
-describe('functions', () => {
-  describe('happyHolidays', () => {
-    it('returns "Happy holidays!"', () => {
-      expect(happyHolidays()).toEqual('Happy holidays!')
-    })
-  })
-
   describe('happyHolidaysTo(name)', () => {
     it('returns "Happy holidays, ${name}!"', () => {
       expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
